5ebc6a01ee6982c40d32efceb10ba94c6ca72c9b
Vijay_Ky <vijayky007@gmail.com>
2021-02-26 16:01:11 +0530

    first commit

---
+++ non-static/app1/src/Q.java
@@ -0,0 +1,15 @@
+class Q
+{
+	int i;
+	public static void main(String[]args)
+	{
+		Q q1 = new Q();
+		q1.i = 10;
+		Q q2 = q1;
+		q2.i = 20;
+		Q q3 = q2;
+		System.out.println(q1.i);
+		System.out.println(q2.i);
+		System.out.println(q3.i);
+	}
+}
---
+++ non-static/app1/src/X.java
@@ -0,0 +1,22 @@
+class X
+{
+	int i;
+	static void test(X obj1, X obj2)
+	{
+		int i = obj1.i;
+		obj1.i = obj2.i;
+		obj2.i = i;
+	}
+	public static void main(String[] args)
+	{
+		X x1 = new X(), x2 = new X();
+		//int i=9, j=20;
+		x1.i = 10;
+		x2.i = 20;
+		System.out.println(x1.i);
+		System.out.println(x2.i);
+		test(x1, x2); 
+		System.out.println(x1.i);
+		System.out.println(x2.i);
+	}
+}
